41dfe49eead16db429e55f1b5950632c6b91edba
Vasyl Kotiai <vasyl.kotiai@ringteam.com>
2024-10-13 23:26:04 +0300

    HW for OOP2 from Kotiai

--- src/ContractedEmployee.java
+++ src/ContractedEmployee.java
@@ -1,36 +1,53 @@
-public class ContractedEmployee {
+public class ContractedEmployee extends Employee {
     // TODO fix class declaration and declare variables here
+    private String federalTaxId;
+    private double hourlyRate;
+    private double hoursWorked;
 
     public ContractedEmployee(String employeeId, String name, String federalTaxId) {
         // TODO fill in code here
+        super(employeeId, name);
+        this.federalTaxId = federalTaxId;
     }
 
     public String getFederalTaxId() {
         // TODO fill in code here and replace the return statement
-        return "";
+        return federalTaxId;
     }
 
     public void setFederalTaxId(String federalTaxId) {
         // TODO fill in code here
+        this.federalTaxId = federalTaxId;
     }
 
     public double getHourlyRate() {
         // TODO fill in code here and replace the return statement
-        return 0;
+        return hourlyRate;
     }
 
     public void setHourlyRate(double hourlyRate) {
         // TODO fill in code here
+        this.hourlyRate = hourlyRate;
     }
 
     public double getNumberOfHoursWorked() {
         // TODO fill in code here and replace the return statement
-        return 0;
+        return hoursWorked;
     }
 
-    public void setNumberOfHoursWorked(double numberOfHoursWorked) {
+    public void setNumberOfHoursWorked(double hoursWorked) {
         // TODO fill in code here
+        this.hoursWorked = hoursWorked;
     }
 
     // TODO fill in code here
+    @Override
+    public double calculatePay() {
+        return hourlyRate * hoursWorked;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + String.format(", Federal Tax ID: %s", federalTaxId);
+    }
 }
