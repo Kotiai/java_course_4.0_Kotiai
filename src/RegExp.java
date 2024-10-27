fcc0d92f128cf501d6731d65ced8dde57c427a90
Vasyl Kotiai <vasyl.kotiai@ringteam.com>
2024-10-27 23:16:23 +0200

    HW10 regexp from Kotiai

--- src/RegExp.java
+++ src/RegExp.java
@@ -1,3 +1,6 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Regular expressions.
  */
@@ -11,6 +14,12 @@ public class RegExp {
      */
     public static String getIpAddress(String text) {
         // TODO fill in code here for extracting IP address using Regular Expression
+        String patternString = "\\b((\\d|[1-9]\\d|1\\d{2}|2[0-5]{2})\\.){3}(\\d|[1-9]\\d|1\\d{2}|2[0-5]{2})\\b";
+        Pattern pattern = Pattern.compile(patternString);
+        Matcher matcher = pattern.matcher(text);
+        if (matcher.find()) {
+            return matcher.group();
+        }
         return "";
     }
 }
--- src/RegExpTest.java
+++ src/RegExpTest.java
@@ -21,8 +21,10 @@ public class RegExpTest {
                 {" Invalid IP: 256.256.256.256!", ""},
                 {"invalid:256.256.256.256", ""},
                 {"valid IP: 255.255.255.255!", "255.255.255.255"},
-                {"invalid:255.255.255.255", "255.255.255.255"}
+                {"invalid:255.255.255.255", "255.255.255.255"},
                 // TODO add 2 more test data here
+                {"The SSID of the device AP is: 192.168.16.1", "192.168.16.1"},
+                {"The SSID of your home AP: 192.168.17.455", ""}
         };
     }
 
