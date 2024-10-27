e9d292f741b57886f6b5a19dfce1c90a50fa71f6
Vasyl Kotiai <vasyl.kotiai@ringteam.com>
2024-10-27 15:22:00 +0200

    HW8-basics loop control from Kotiai

--- src/SumAverage.java
+++ src/SumAverage.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Compute the sum and average for running integers using loop.
  */
@@ -12,7 +14,11 @@ public class SumAverage {
      */
     public static int sum(int lowerBound, int upperBound) {
         // TODO fill in code here using for loop and replace the return statement
-        return -1;
+        int sum = 0;
+        for (int i = lowerBound; i <= upperBound; i++) {
+            sum += i;
+        }
+        return sum;
     }
 
     /**
@@ -23,7 +29,11 @@ public class SumAverage {
      */
     public static int sum(int[] numbers) {
         // TODO fill in code here using for each loop and replace the return statement
-        return -1;
+        int sum = 0;
+        for (int i : numbers) {
+            sum += i;
+        }
+        return sum;
     }
 
     /**
@@ -35,7 +45,14 @@ public class SumAverage {
      */
     public static double average(int lowerBound, int upperBound) {
         // TODO fill in code here using while loop and replace the return statement
-        return -1;
+        int i = lowerBound;
+        int sum = 0;
+        while (i <= upperBound){
+            sum += i;
+            i++;
+        }
+        double result = (double) sum / (upperBound - lowerBound + 1);
+        return result;
     }
 
     /**
@@ -46,6 +63,12 @@ public class SumAverage {
      */
     public static double average(int[] numbers) {
         // TODO fill in code here using do-while loop and replace the return statement
-        return -1;
+        int sum = 0;
+        int i = 0;
+        do {
+            sum += numbers[i];
+            i++;
+        } while (i < numbers.length);
+        return (double) sum / numbers.length;
     }
 }
--- src/SumAverageTest.java
+++ src/SumAverageTest.java
@@ -17,8 +17,10 @@ public class SumAverageTest {
         return new Object[][]{
                 {1, 100, 5050},
                 {-100, 100, 0},
-                {1, 1, 1}
+                {1, 1, 1},
                 // TODO add 2 more test data here
+                {50, 60, 605},
+                {0, 10, 55}
         };
     }
 
@@ -32,8 +34,10 @@ public class SumAverageTest {
         return new Object[][]{
                 {1, 100, 50.5},
                 {-100, 100, 0.0},
-                {1, 1, 1.0}
+                {1, 1, 1.0},
                 // TODO add 2 more test data here
+                {50, 60, 55.0},
+                {0, 10, 5.0}
         };
     }
 
