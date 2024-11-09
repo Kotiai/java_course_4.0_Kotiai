3c401efaf4f8a712df45756a99ff3a4c7c435611
Vasyl Kotiai <vasyl.kotiai@ringteam.com>
2024-11-09 14:50:21 +0200

    Homework_12_exceptions from Kotiai

--- src/ExampleExceptionTest.java
+++ src/ExampleExceptionTest.java
@@ -15,8 +15,10 @@ public class ExampleExceptionTest {
     public static Object[][] data() {
         return new Object[][]{
                 {2, 2, 4},
-                {2, 3, 6}
+                {2, 3, 6},
                 // TODO add 2 more test data here
+                {7, 3, 21},
+                {3, 4, 12}
         };
     }
 
@@ -29,19 +31,28 @@ public class ExampleExceptionTest {
     public static Object[][] negativeData() {
         return new Object[][]{
                 {-2, 2},
-                {2, -2}
+                {2, -2},
                 // TODO add 2 more test data here
+                {1, -1},
+                {-10, 5}
         };
     }
 
     @Test(dataProvider = "data")
     public void testRectangleArea(int a, int b, int c) {
         // TODO put your code here
+        assertEquals(ExampleException.rectangleArea(a, b), c);
     }
 
 
     @Test(dataProvider = "negativeData")
     public void testRectangleAreaNegative(int a, int b) {
         // TODO put your code here
+        try {
+            ExampleException.rectangleArea(a, b);
+            fail("Expected IllegalArgumentException was not thrown");
+        } catch (IllegalArgumentException e) {
+            assertEquals(e.getMessage(), "input value is below zero!");
+        }
     }
 }
