80c74d9e2bb5d4a3f3533c8438f612b0eb574e6b
Vasyl Kotiai <vasyl.kotiai@ringteam.com>
2024-11-10 19:30:51 +0200

    Homework_13_lambda from Kotiai

--- src/LambdaTest.java
+++ src/LambdaTest.java
@@ -15,8 +15,10 @@ public class LambdaTest {
                 {-1.0, 10.0, 10.0},
                 {0.0, 0.0, 0.0},
                 {-10.0, NaN, NaN},
-                {POSITIVE_INFINITY, 1e32, POSITIVE_INFINITY}
+                {POSITIVE_INFINITY, 1e32, POSITIVE_INFINITY},
                 // TODO add 2 more test data here
+                {-12.0, -21.0, -12.0},
+                {121.0, -121.0, 121.0}
         };
     }
 
@@ -25,8 +27,10 @@ public class LambdaTest {
         return new Object[][]{
                 {25.0, 5.0},
                 {-1.0, NaN},
-                {POSITIVE_INFINITY, POSITIVE_INFINITY}
+                {POSITIVE_INFINITY, POSITIVE_INFINITY},
                 // TODO add 2 more test data here
+                {49.0, 7.0},
+                {121.0, 11.0}
         };
     }
 
