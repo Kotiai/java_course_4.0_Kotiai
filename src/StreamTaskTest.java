bd3352030ff7775daa8ef03a89fd47ce32d7c2b6
Vasyl Kotiai <vasyl.kotiai@ringteam.com>
2024-11-10 20:04:13 +0200

    Homework_14_Stream from Kotiai

--- src/StreamTaskTest.java
+++ src/StreamTaskTest.java
@@ -19,8 +19,16 @@ public class StreamTaskTest {
                 {
                         new int[]{13, -2, 33, -4, 5},
                         20
-                }
+                },
                 // TODO add 2 more test data here
+                {
+                        new int[]{3, -6, -4, -4, 8},
+                        132
+                },
+                {
+                        new int[]{12, -8, 7, 21, 10, -1, -11, -14},
+                        504
+                },
         };
     }
 
@@ -38,8 +46,16 @@ public class StreamTaskTest {
                 {
                         new int[]{130, -200, 330, -40, 50, 66},
                         new int[]{}
-                }
-                // TODO add 2 more test data here
+                },
+                // TODO add 2 more test data
+                {
+                        new int[]{13, -11, 4, 7, 12, 21},
+                        new int[]{-11, 7, 13, 21}
+                },
+                {
+                        new int[]{122, -23, 49, -17, -33, 41},
+                        new int[]{-33, -23, -17, 41, 49}
+                },
         };
     }
 
